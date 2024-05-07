faa3e6bb9407aac322ee0b6f0117a940b76d1c94
GreatnessGrace <graceunstoppable7@gmail.com>
2024-05-07 13:02:52 +0530

    [WIP] Verify OTP

--- src/app/Signup.tsx
+++ src/app/Signup.tsx
@@ -9,7 +9,7 @@ export default function Signup() {
         <form>
           <div className="mb-4">
             <label htmlFor="name" className="block text-sm font-medium text-gray-700">Name</label>
-            <input type="text" id="name" name="name" className="mt-1 p-2 border border-gray-300 rounded-md w-full" placeholder="Enter" />
+            <input type="text" id="name" placeholder="Enter" name="name" className="mt-1 p-2 border border-gray-300 rounded-md w-full" />
           </div>
           <div className="mb-4">
             <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
@@ -25,10 +25,23 @@ export default function Signup() {
         </form>
         <p className="mt-4 text-center text-gray-600">
           Have an Account?{" "}
-          <Link href="/login" className=" text-lg hover:underline text-black">&nbsp;&nbsp;LOGIN
+          <Link 
+          href="/login" className=" text-lg hover:underline text-black">&nbsp;&nbsp;LOGIN
           </Link>
         </p>
       </div>
-    </div> 
+      <div className="m-5 flex items-center justify-center h/2-screen">
+      <div className="w-116 p-8 border border-gray-300 rounded-xl">
+      <h1 className="text-2xl font-bold mb-4 flex items-center justify-center">Verify your Email</h1>
+
+      <h3 className="text-sm mb-4 flex items-center justify-center">Enter the 8 digit code you recieved on</h3>
+      
+      <h3 className=" font-bold text-sm mb-4 flex items-center justify-center">ankush***@gmail.com</h3>
+      <h2 className="text-sm mb-4 flex items-center justify-start">Code</h2>
+      <button type="submit" className="w-full py-2 px-4 bg-black text-white rounded-md hover:bg-gray-800">Verify
+      </button>
+      </div>
+      </div>
+    </div>
   ); 
 }
