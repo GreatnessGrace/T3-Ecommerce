c6f02648e58d46f27d9d2369b74327d9750d93a3
GreatnessGrace <graceunstoppable7@gmail.com>
2024-05-08 17:47:14 +0530

    [WIP] Categories

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { api } from "~/trpc/server";
 import { Header } from "~/app/_components/header";
 import Signup from "./Signup";
 import Login from "./Login";
+import Categories from "./Categories";
 export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
 
@@ -13,6 +14,7 @@ export default async function Home() {
     <Header/>
     <Signup/>
     <Login/>
+    <Categories/>
 
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
